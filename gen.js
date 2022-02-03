9f9e4a4b265593bedacb9565a529e318f984797b
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2022-02-03 12:42:59 +0100

    Initial commit

---
+++ gen.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+var max = 101;
+
+fs.writeFileSync("mycalc.js", `
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question("Whats your first number?", (num1) => {
+        if(num1 > ${max}) {
+            console.log("Error: Exceeds max of ${max}");
+            process.exit(1);
+        }
+
+        rl.question("What operator do you want to use?", (op) => {
+            if(!(op == "+" || op == "-" || op == "*" || op == "/")) {
+                console.log("Error: Unsupported operator!!");
+                process.exit(1);
+            }
+
+            rl.question("Whats your second number?", (num2) => {
+
+`);
+
+for(var i = 0; i < max; i++) {
+    for(var j = 0; j < max; j++) {
+        fs.appendFileSync("mycalc.js", `if(num1 == ${i} && op == "+" && num2 == ${j}) console.log("The correct answer is: ${i + j}");\n`);
+        fs.appendFileSync("mycalc.js", `if(num1 == ${i} && op == "-" && num2 == ${j}) console.log("The correct answer is: ${i - j}");\n`);
+        fs.appendFileSync("mycalc.js", `if(num1 == ${i} && op == "*" && num2 == ${j}) console.log("The correct answer is: ${i * j}");\n`);
+        fs.appendFileSync("mycalc.js", `if(num1 == ${i} && op == "/" && num2 == ${j}) console.log("The correct answer is: ${i / j}");\n`);
+    }
+}
+
+fs.appendFileSync("mycalc.js", `});
+    });
+});`);
